47781b6bf1b11547871adfb160a940ecd3dac3c5
IPad4610sabu-2 <aihfhauu@gmail.com>
2025-01-06 11:49:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ app.get('/w/:id/5.pdf', async (req, res) => {
         res.render('Yukiyoutube-mod', { videoData, videoId });
   } catch (error) {
         res.status(500).render('matte', { 
-      videotire,
       videoId, 
       error: '動画を取得できません', 
       videotire: 'nodeta', 

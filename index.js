14603a47e47bed62f3b23022502b7c1d647ce02c
IPad4610sabu-2 <aihfhauu@gmail.com>
2025-01-06 20:06:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/w/:id/5.pdf', async (req, res) => {
         res.status(500).render('matte', { 
       videoId, 
       error: '動画を取得できません', 
-      videotitle: '${title}', 
+      videotitle: '{videotitle}', 
       videoid: '${videoId}', 
       details: error.message
     });
